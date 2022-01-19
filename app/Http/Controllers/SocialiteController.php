4393451a1225799632a5a1c579806c64ffbdaa5c
Your Name <kholoudbatis@gmail.com>
2022-01-19 11:54:33 +0300

    Initial commit

---
+++ app/Http/Controllers/SocialiteController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Laravel\Socialite\Facades\Socialite;
+
+class SocialiteController extends Controller
+{
+   public function redirect($service){
+
+   return Socialite::driver($service)->redirect();
+
+   }
+      public function callback($service){
+
+     return  $user = Socialite::driver($service) -> user();
+
+
+      }
+}
